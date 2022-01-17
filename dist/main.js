ab9c287666f209a37032acb00844aeec529da7a9
Jef Ogues <jef.ogues_@hotmail.com>
2022-01-17 10:26:52 -0500

    finished project V. 1

---
+++ dist/main.js
@@ -0,0 +1,22 @@
+
+/******/ (() => { 
+	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("const apiKey = '13cff259275e4dfe9a0155838211603';\r\nconst cityName = document.querySelector('h1');\r\nconst temph2 = document.querySelector('h2');\r\nconst conditionh3 = document.querySelector('h3')\r\nconst conditionimg = document.querySelector('img');\r\nconst highLowP = document.getElementById('highLow');\r\nconst moreInfo = document.getElementById('moreInfo');\r\nconst form = document.querySelector('form');\r\nconst button = document.querySelector('button');\r\nconst toggleC = document.getElementById('Cel');\r\nconst toggleF = document.getElementById('Fah');\r\n\r\nlet newCity = \"toronto\";\r\nlet tempType = \"F\";\r\n\r\nform.addEventListener('submit', handlesubmit);\r\nbutton.addEventListener('click', handlesubmit);\r\n\r\ntoggleC.addEventListener('click', function () {\r\n    tempType = \"C\"\r\n    getCity(newCity);\r\n});\r\n\r\ntoggleF.addEventListener('click', function(){\r\n    tempType = \"F\"\r\n    getCity(newCity);\r\n});\r\n\r\nasync function getCity(city) {\r\n    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=1`, {\r\n        mode: 'cors'\r\n    });\r\n    try {\r\n        const cityInfo = await response.json();\r\n        if (cityInfo.error) {\r\n            alert(cityInfo.error.message);\r\n        }\r\n        console.log(cityInfo);\r\n        displayWeather(cityInfo);\r\n    } catch (error) {\r\n\r\n    }\r\n}\r\n\r\nfunction displayWeather(data) {\r\n\r\n    let city = data.location.name;\r\n    let condition = data.current.condition.text;\r\n    let conditionPNG = data.current.condition.icon;\r\n\r\n    displayTemp(data)\r\n    cityName.innerHTML = city;\r\n    conditionh3.innerHTML = condition;\r\n    conditionimg.src = conditionPNG;\r\n}\r\n\r\nfunction displayTemp(data){\r\n    let humidity = data.current.humidity;\r\n    let sunrise = data.forecast.forecastday[0].astro.sunrise;\r\n    let sunset = data.forecast.forecastday[0].astro.sunset;\r\n    let temp = {\r\n        c: data.current.temp_c,\r\n        f: data.current.temp_f\r\n    };\r\n    let highTemp = {\r\n        c: data.forecast.forecastday[0].day.maxtemp_c,\r\n        f: data.forecast.forecastday[0].day.maxtemp_f\r\n    };\r\n    let lowTemp = {\r\n        c: data.forecast.forecastday[0].day.mintemp_c,\r\n        f: data.forecast.forecastday[0].day.mintemp_f\r\n    };\r\n    let feelsLike = {\r\n        c: data.current.feelslike_c,\r\n        f: data.current.feelslike_c\r\n    };\r\n    let windK = {\r\n        k: data.current.wind_kph,\r\n        m: data.current.wind_mph\r\n    };\r\n    let visibility = {\r\n        k: data.current.vis_km,\r\n        m: data.current.vis_miles\r\n    };\r\n\r\n    switch (tempType) {\r\n        case \"C\":\r\n            temph2.innerHTML = `${temp.c}&degC`;\r\n            highLowP.innerHTML = `H:${highTemp.c}&degC | L:${lowTemp.c}&degC`;\r\n            moreInfo.innerHTML = `Feels Like: ${feelsLike.c}&degC | Wind: ${windK.k} km/hr | Humidity: ${humidity}% |\r\n            Visibility: ${visibility.k} km <br> Sunrise: ${sunrise} | Sunset: ${sunset}`;\r\n            break;\r\n        case \"F\":\r\n            temph2.innerHTML = `${temp.f}&degF`;\r\n            highLowP.innerHTML = `H:${highTemp.f}&degF | L:${lowTemp.f}&degF`;\r\n            moreInfo.innerHTML = `Feels Like: ${feelsLike.f}&degF | Wind: ${windK.m} m/hr | Humidity: ${humidity}% |\r\n            Visibility: ${visibility.m} miles <br> Sunrise: ${sunrise} | Sunset: ${sunset}`;\r\n    }\r\n}\r\n\r\n\r\nfunction handlesubmit(e) {\r\n    e.preventDefault();\r\n    const input = document.querySelector('input[type=\"text\"]')\r\n    newCity = input.value;\r\n    getCity(newCity);\r\n    form.reset();\r\n}\r\n\r\nwindow.onload = getCity(newCity);\n\n//# sourceURL=webpack://odin-weather/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ src/index.js
@@ -0,0 +1,110 @@
+const apiKey = '13cff259275e4dfe9a0155838211603';
+const cityName = document.querySelector('h1');
+const temph2 = document.querySelector('h2');
+const conditionh3 = document.querySelector('h3')
+const conditionimg = document.querySelector('img');
+const highLowP = document.getElementById('highLow');
+const moreInfo = document.getElementById('moreInfo');
+const form = document.querySelector('form');
+const button = document.querySelector('button');
+const toggleC = document.getElementById('Cel');
+const toggleF = document.getElementById('Fah');
+
+let newCity = "toronto";
+let tempType = "F";
+
+form.addEventListener('submit', handlesubmit);
+button.addEventListener('click', handlesubmit);
+
+toggleC.addEventListener('click', function () {
+    tempType = "C"
+    getCity(newCity);
+});
+
+toggleF.addEventListener('click', function(){
+    tempType = "F"
+    getCity(newCity);
+});
+
+async function getCity(city) {
+    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=1`, {
+        mode: 'cors'
+    });
+    try {
+        const cityInfo = await response.json();
+        if (cityInfo.error) {
+            alert(cityInfo.error.message);
+        }
+        console.log(cityInfo);
+        displayWeather(cityInfo);
+    } catch (error) {
+
+    }
+}
+
+function displayWeather(data) {
+
+    let city = data.location.name;
+    let condition = data.current.condition.text;
+    let conditionPNG = data.current.condition.icon;
+
+    displayTemp(data)
+    cityName.innerHTML = city;
+    conditionh3.innerHTML = condition;
+    conditionimg.src = conditionPNG;
+}
+
+function displayTemp(data){
+    let humidity = data.current.humidity;
+    let sunrise = data.forecast.forecastday[0].astro.sunrise;
+    let sunset = data.forecast.forecastday[0].astro.sunset;
+    let temp = {
+        c: data.current.temp_c,
+        f: data.current.temp_f
+    };
+    let highTemp = {
+        c: data.forecast.forecastday[0].day.maxtemp_c,
+        f: data.forecast.forecastday[0].day.maxtemp_f
+    };
+    let lowTemp = {
+        c: data.forecast.forecastday[0].day.mintemp_c,
+        f: data.forecast.forecastday[0].day.mintemp_f
+    };
+    let feelsLike = {
+        c: data.current.feelslike_c,
+        f: data.current.feelslike_c
+    };
+    let windK = {
+        k: data.current.wind_kph,
+        m: data.current.wind_mph
+    };
+    let visibility = {
+        k: data.current.vis_km,
+        m: data.current.vis_miles
+    };
+
+    switch (tempType) {
+        case "C":
+            temph2.innerHTML = `${temp.c}&degC`;
+            highLowP.innerHTML = `H:${highTemp.c}&degC | L:${lowTemp.c}&degC`;
+            moreInfo.innerHTML = `Feels Like: ${feelsLike.c}&degC | Wind: ${windK.k} km/hr | Humidity: ${humidity}% |
+            Visibility: ${visibility.k} km <br> Sunrise: ${sunrise} | Sunset: ${sunset}`;
+            break;
+        case "F":
+            temph2.innerHTML = `${temp.f}&degF`;
+            highLowP.innerHTML = `H:${highTemp.f}&degF | L:${lowTemp.f}&degF`;
+            moreInfo.innerHTML = `Feels Like: ${feelsLike.f}&degF | Wind: ${windK.m} m/hr | Humidity: ${humidity}% |
+            Visibility: ${visibility.m} miles <br> Sunrise: ${sunrise} | Sunset: ${sunset}`;
+    }
+}
+
+
+function handlesubmit(e) {
+    e.preventDefault();
+    const input = document.querySelector('input[type="text"]')
+    newCity = input.value;
+    getCity(newCity);
+    form.reset();
+}
+
+window.onload = getCity(newCity);
